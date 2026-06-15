0ec53040ba7a705d270093b422bd4d14ac370b75
aryopratamaa <mr.aryopratama@gmail.com>
2026-06-15 11:11:53 +0700

    11.11

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PlantController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\LaporanController;
 
 Route::get('/', function () {
     return view('dashboard');
-});
+})->name('dashboard');
 
-Route::get('/kategori', function () {
-    return view('kategori');
-});
-
-Route::get('/laporan', function () {
-    return view('laporan');
-});
-
-Route::get('/tanaman', function () {
-    return view('tanaman');
-});
-
-Route::get('/event', function () {
-    return view('event');
-});
+Route::resource('kategori', KategoriController::class);
+Route::resource('tanaman', PlantController::class);
+Route::resource('event', EventController::class);
+Route::resource('laporan', LaporanController::class);
